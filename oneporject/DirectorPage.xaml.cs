e4139f28e7314439d6c2688fe132edaab5cddabb
Merrcurys <a.sebezhko2017@gmail.com>
2024-03-29 23:55:59 +0300

    first practica

---
+++ oneporject/DirectorPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data;
+using oneporject.FactoryTableAdapters;
+
+namespace oneporject
+{
+    /// <summary>
+    /// Логика взаимодействия для DirectorPage.xaml
+    /// </summary>
+    public partial class DirectorPage : Page
+    {
+        // DataSet
+        EmployeesTableAdapter employee = new EmployeesTableAdapter();
+        DirectorsTableAdapter director = new DirectorsTableAdapter();
+        public DirectorPage()
+        {
+            InitializeComponent();
+
+            directorDgr.ItemsSource = director.GetData();
+        }
+
+        private void directorDgr_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var strochka = (directorDgr.SelectedItem as DataRowView).Row;
+            NameTbx.Text = strochka[2].ToString();
+        }
+    }
+}
---
+++ oneporject/EmployeePage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace oneporject
+{
+    /// <summary>
+    /// Логика взаимодействия для EmployeePage.xaml
+    /// </summary>
+    public partial class EmployeePage : Page
+    {
+        // EntityFramework
+        FactoryEntities db = new FactoryEntities(); // FactoryEntities
+        public EmployeePage()
+        {
+            InitializeComponent();
+
+            employeDgr.ItemsSource = db.Employees.ToList();
+            DirectorCbx.ItemsSource = db.Directors.ToList();
+        }
+
+        private void employeDgr_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var strochka = employeDgr.SelectedItem as Employees;
+            NameTbx.Text = strochka.Surname_Employee;
+            DirectorCbx.SelectedItem = strochka.Directors;
+        }
+    }
+}
